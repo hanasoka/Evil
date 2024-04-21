14ded505f025f31b4cf928efd3fe92aebfeaa7f9
Skylar Asoka <57451015+skylarasoka@users.noreply.github.com>
2024-04-21 13:12:04 +0000

    added files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from website import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/')
-def home():
-  return "Hello World"
+if __name__ == '__main__':
+    app.run(debug=True)
+  
